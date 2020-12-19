5817d4e54a6fac94c099427661d6d57de8b7092b
yakol <ya.kolysh@gmail.com>
2020-12-19 21:31:11 +0300

    Initial commit

---
+++ src/mirea/l2/Main.java
@@ -0,0 +1,7 @@
+package mirea.l2;
+
+public class Main {
+    public static void main(String[] args) {
+        Window w = new Window();
+    }
+}
---
+++ src/mirea/l3/Main.java
@@ -0,0 +1,7 @@
+package mirea.l3;
+
+public class Main {
+    public static void main(String[] args) {
+        Window w = new Window();
+    }
+}
